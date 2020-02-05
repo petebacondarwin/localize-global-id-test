abc75bc50ad4397538bdbd5cf48006d4e6bd4553
Pete Bacon Darwin <pete@bacondarwin.com>
2020-02-05 10:45:12 +0000

    add localization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'localize-global-id-test';
+  title = $localize `:@@loginError:Original`;
 }
